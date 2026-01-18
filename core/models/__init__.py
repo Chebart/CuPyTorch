09f4b0be81c5f62f5fcef0ca3691429df9f65e2c
Artyom Chebykin <chebart03@gmail.com>
2026-01-18 16:08:11 +0300

    updated models

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -5,3 +5,4 @@ from .rnn import RNN, RNNClsf, RNNReg
 from .lstm import LSTM, LSTMReg
 from .gru import GRU, GRUReg
 from .vae_gan import VAEEncoder, Generator, Discriminator
+from .bert import BERT
