4a8346e1930db62b8d17f60fb6330c06fbafe19f
Artyom Chebykin <chebart03@gmail.com>
2026-01-19 17:25:49 +0300

    fixed bugs in CrossEntropyLoss

--- core/losses/cross_entropy_loss.py
+++ core/losses/cross_entropy_loss.py
@@ -7,44 +7,38 @@ class CrossEntropyLoss(AbstractLoss):
     def __init__(self, model, ignore_index = None):
         super().__init__(model)
         self.ignore_index = ignore_index
-
-    def _convert_labels_to_one_hot(self, num_cls, labels):
-        return Tensor.eye(num_cls, dtype=labels.dtype, device=labels.device)[labels]
+        self.valid_mask = None
 
     def forward(self, y_pred, y_true):
-        # Ignore index if needed
-        if self.ignore_index is not None:
-            valid_mask = y_true != self.ignore_index
-            y_pred = y_pred[valid_mask]
-            y_true = y_true[valid_mask]
+        # Ignore index if set
+        if self.ignore_index is None:
+            self.valid_mask = Tensor.ones(y_true.shape, device = y_pred.device).astype("bool")
+        else:
+            self.valid_mask = (y_true != self.ignore_index).astype("bool")
+            
+        # Use mask to get true class
+        y_pred = y_pred[self.valid_mask]
+        y_true = y_true[self.valid_mask]
 
         # Return 0 if everything is ignored
         if y_true.shape[0] == 0:
             return Tensor(0.0, dtype = y_pred.dtype, device = y_pred.device)
-        
-        # Calculate loss
-        y_true_oh = self._convert_labels_to_one_hot(y_pred.shape[1], y_true)
-        loss = -(y_true_oh * (y_pred + self.eps).log()).sum() / y_true_oh.shape[0]
 
-        return loss
+        # Select predicted probability of true class
+        self.idx = y_true.astype("int32")
+        self.batch_idx = Tensor.arange(len(self.idx), device = y_pred.device).astype("int32")
+        true_probs = y_pred[self.batch_idx, self.idx]
 
-    def backward(self, y_pred, y_true):
-        # If ignore_index is set
-        if self.ignore_index is not None:
-            dLdy_full = Tensor.zeros(y_pred.shape, dtype = y_pred.dtype, device = y_pred.device)
-            valid_mask = y_true != self.ignore_index
-            y_pred_valid = y_pred[valid_mask]
-            y_true_valid = y_true[valid_mask]
+        return -(true_probs + self.eps).log().mean()
 
-            if y_true_valid.shape[0] == 0:
-                return dLdy_full
+    def backward(self, y_pred, y_true):  
+        # Create full matrix
+        dLdy = Tensor.zeros(y_pred.shape, dtype=y_pred.dtype, device=y_pred.device)
 
-            y_true_oh = self._convert_labels_to_one_hot(y_pred_valid.shape[1], y_true_valid)
-            dLdy_valid = -y_true_oh / (y_pred_valid * y_true_oh.shape[0] + self.eps)
-            dLdy_full[valid_mask] = dLdy_valid
-            dLdy = dLdy_full
-        else:
-            y_true_oh = self._convert_labels_to_one_hot(y_pred.shape[1], y_true)
-            dLdy = -y_true_oh / (y_pred * y_true_oh.shape[0] + self.eps)
+        # Calculate gradient
+        y_pred_f = y_pred[self.valid_mask]
+        B = y_pred_f.shape[0]
+        grad = -1 / (y_pred_f[self.batch_idx, self.idx] * B + self.eps)
+        dLdy[self.valid_mask, self.idx] = grad
 
         return self.model.backward(dLdy)
